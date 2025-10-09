3cbcfb6d8292071af32e246cfe65795bfd4f2d05
Daniel-2473 <daniel.ulate937@gmail.com>
2025-10-09 10:58:27 -0700

    Cambios posicion

--- RegisterScene.py
+++ RegisterScene.py
@@ -85,21 +85,16 @@ class RegisterScene(Scene):
         self.aboutButton = ImageButton(140, 40, "aboutButton.png", 0.15)
         self.checkBox = ImageButton(screenW//2 - 375, yStart + ySpacing * (totalRows + 1), "checkboxBlank.png", 0.05, "checkboxFull.png")
 
-<<<<<<< Updated upstream
-        self.subscribeButton = Button(screenW // 2, 
+        # Botón de suscribirse centrado
+        self.subscribeButton = Button(screenW // 2 - self.fieldWidth // 2, 
                                       bottomY + 100, 
                                       self.fieldWidth, self.fieldHeight,
-                                      "Suscribirse", font, (0, 0, 0), (91, 81, 81), (192, 58, 48))
+                                      "Suscribirse", font, (0, 0, 0), (91, 81, 81), (58, 192, 48))
 
         self.buttonsList = [self.registerButton, self.loginButton, self.faceRecognitionButton,
                             self.helpButton, self.aboutButton, self.checkBox, self.subscribeButton]
 
         self.termsAndConditions = SimpleText("He leído y acepto los términos y condiciones",
-=======
-        self.buttonsList = [self.registerButton, self.loginButton, self.faceRecognitionButton,
-                            self.helpButton, self.aboutButton, self.checkBox]
-        self.termsAndConditions = SimpleText("I have read and accept the terms and conditions",
->>>>>>> Stashed changes
                                              screenW // 2, yStart + ySpacing * (totalRows + 1),
                                              font, (0, 0, 0))
 
@@ -127,16 +122,12 @@ class RegisterScene(Scene):
 
         for box in self.fields:
             box.handleEvent(event, scrollOffset=-self.scrollY)
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
         for button in self.buttonsList:
             if button.wasClicked(event, scrollOffset=-self.scrollY):
                 if button == self.registerButton:
                     data = {box.text: box.getText() for box in self.fields}
                     print("Datos registrados:", data)
-<<<<<<< Updated upstream
                 elif button == self.loginButton:
                     self.switchScene("login")
                 elif button == self.subscribeButton:
@@ -147,16 +138,6 @@ class RegisterScene(Scene):
         if self.termsAndConditions.wasClicked(event, scrollOffset=-self.scrollY):
             self.openPdf("TerminosCondicionesTecnolators.pdf")
 
-=======
-                if button == self.loginButton:
-                    self.switchScene("login")
-                if button == self.checkBox:
-                    self.openPdf("TerminosCondicionesTecnolators.pdf")
-        if self.termsAndConditions.wasClicked(event, scrollOffset=-self.scrollY):
-            self.openPdf("TerminosCondicionesTecnolators.pdf")
-
-
->>>>>>> Stashed changes
     def update(self, deltaTime):
         mousePos = pygame.mouse.get_pos()
         for box in self.fields:
@@ -173,10 +154,6 @@ class RegisterScene(Scene):
         for button in self.buttonsList:
             button.draw(screen, scrollOffset=offset)
         self.termsAndConditions.draw(screen, scrollOffset=offset)
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
         for box in self.fields:
             if isinstance(box, DropdownButton):
                 box.draw(screen, deltaTime=0, scrollOffset=offset)
