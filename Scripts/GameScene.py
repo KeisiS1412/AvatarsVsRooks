949c841469f06c831817292c82bcc128e321585c
Daniel-2473 <daniel.ulate937@gmail.com>
2025-10-24 20:59:24 -0600

    Create GameScene.py

---
+++ Scripts/GameScene.py
@@ -0,0 +1,8 @@
+import pygame
+import Scene
+
+class GameScene(Scene):
+    """Escena de registro con campos de texto, menús desplegables y botones."""
+
+    def __init__(self):  # Inicializa la escena
+        pass
