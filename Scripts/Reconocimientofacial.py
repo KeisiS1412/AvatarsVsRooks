4a0946175b2eb03314c58bc02e804dc84642700e
Keisi Solano <k.solano.6@estudiantec.cr>
2025-10-30 12:40:42 -0600

    Implementación de imagen en registro

--- Scripts/Reconocimientofacial.py
+++ Scripts/Reconocimientofacial.py
@@ -216,4 +216,6 @@ class ReconocimientoFacialLBPH:
         self.running = False
         pygame.quit()
 
-
+if __name__ == "__main__":  # [FIX]
+    app = ReconocimientoFacialLBPH(usuario_actual="el_mas_pro")  # [FIX] puedes poner aquí el usuario activo
+    app.menu()
